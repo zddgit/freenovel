049e3e63c77a0fb79b0f1265944686c469891c61
zdd <583627140@qq.com>
2018-10-26 17:03:53 +0800

    把一些掺杂在视图结构里面的方法抽离出来

--- lib/views/ChapterDetail.dart
+++ lib/views/ChapterDetail.dart
@@ -19,33 +19,27 @@ class ChapterDetail extends StatefulWidget {
   }
 }
 
+
+
+
+
+
+
 class _ChapterDetailState extends State<ChapterDetail> {
+  SharedPreferences prefs;
   /// 当前所读小说的id和章节id
   final int novelId;
   int currentChapterId;
-
   /// 目录章节标题
   List<Chapter> titles;
-
   /// 当前正在读的章节
   LimitQueue<Chapter> readChapters;
-
   /// 滚动控制
   ScrollController scrollController;
-
-  SharedPreferences prefs;
-
   /// 控制是不是点击目录产生的跳转
-  bool onclick=false;
-
+  bool onclick = false;
 
   _ChapterDetailState(this.novelId, this.currentChapterId);
-
-  updateUI({fn}) {
-    setState(() {
-      if (fn != null) fn();
-    });
-  }
 
   @override
   void initState() {
@@ -57,33 +51,34 @@ class _ChapterDetailState extends State<ChapterDetail> {
     initPrefs();
   }
 
-  initPrefs() async {
-    prefs = await SharedPreferences.getInstance();
-  }
-
   @override
   void dispose() {
     super.dispose();
-
     /// 退出阅读页面的时候保存阅读信息
     String key = NovelStatus.getReadStatusPrefsKey(novelId);
-//    Map map = {};
-//    map["currentChapterId"] = currentChapterId;
-//    prefs.setString(key, json.encode(map));
     prefs.setInt(key, currentChapterId);
-
   }
-
-  void getTitles() async {
+  getTitles() async {
     String titlesJsonStr = await HttpUtil.get(NovelAPI.getTitles(novelId));
     List list = json.decode(titlesJsonStr);
     if (titles == null) titles = [];
     list.forEach((item) {
-      titles.add(Chapter(item['chapterId'], item['novelId'], "第${item['chapterId']}章 " + item['title']));
+      titles.add(Chapter(item['chapterId'], item['novelId'],
+          "第${item['chapterId']}章 " + item['title']));
     });
-    readChapters.addLast( titles.elementAt(currentChapterId == null ? 0 : currentChapterId - 1));
+    readChapters.addLast(
+        titles.elementAt(currentChapterId == null ? 0 : currentChapterId - 1));
     getNovelDetail(readChapters.elementAt(0));
     updateUI();
+  }
+  initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  updateUI({fn}) {
+    setState(() {
+      if (fn != null) fn();
+    });
   }
 
   @override
@@ -113,48 +108,9 @@ class _ChapterDetailState extends State<ChapterDetail> {
         ),
         body: Builder(builder: (BuildContext context) {
           return GestureDetector(
-              onVerticalDragDown: (_) {
-                onclick = false;
-                // 当读到最后一章得时候进行加载
-                // 这里指定快划到最后150像素的时候，进行加载
-                if (scrollController.position.maxScrollExtent - scrollController.offset < 150 && currentChapterId < titles.length) {
-                  Chapter ch = Chapter(currentChapterId + 1, novelId, titles[currentChapterId].title);
-                  readChapters.addLast(ch);
-                  getNovelDetail(ch);
-                  currentChapterId++;
-                } else if (currentChapterId == titles.length && scrollController.position.maxScrollExtent - scrollController.offset < 50) {
-                  Fluttertoast.showToast(
-                      msg: "已经最后一章了",
-                      toastLength: Toast.LENGTH_SHORT,
-                      gravity: ToastGravity.BOTTOM,
-                      timeInSecForIos: 2,
-                      bgcolor: "#777777",
-                      textcolor: '#ffffff');
-                }
-              },
+              onVerticalDragDown: onVerticalDragDown,
               child: RefreshIndicator(
-                onRefresh: () {
-                  if(onclick){
-                    return Future(() { });
-                  }
-                  currentChapterId = readChapters.elementAt(0).chapterId;
-                  if (currentChapterId >= 2) {
-                    currentChapterId--;
-                    Chapter ch = Chapter(currentChapterId, novelId, titles[currentChapterId - 1].title);
-                    readChapters.addFirst(ch);
-                    return getNovelDetail(ch);
-                  } else {
-                    return Future(() {
-                      Fluttertoast.showToast(
-                          msg: "已经是第一章了",
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.BOTTOM,
-                          timeInSecForIos: 2,
-                          bgcolor: "#777777",
-                          textcolor: '#ffffff');
-                    });
-                  }
-                },
+                onRefresh: onRefresh,
                 child: ListView.builder(
                   controller: scrollController,
                   itemCount: readChapters == null ? 0 : readChapters.getLength(),
@@ -162,6 +118,50 @@ class _ChapterDetailState extends State<ChapterDetail> {
                 ),
               ));
         }));
+  }
+
+  Future<void> onRefresh() {
+    if (onclick) {
+      return Future(() {});
+    }
+    currentChapterId = readChapters.elementAt(0).chapterId;
+    if (currentChapterId >= 2) {
+      currentChapterId--;
+      Chapter ch = Chapter( currentChapterId, novelId, titles[currentChapterId - 1].title);
+      readChapters.addFirst(ch);
+      return getNovelDetail(ch);
+    } else {
+      return Future(() {
+        Fluttertoast.showToast(
+            msg: "已经是第一章了",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIos: 2,
+            bgcolor: "#777777",
+            textcolor: '#ffffff');
+      });
+    }
+  }
+
+  onVerticalDragDown(DragDownDetails _) {
+    onclick = false;
+    // 当读到最后一章得时候进行加载
+    // 这里指定快划到最后150像素的时候，进行加载
+    double threshold = scrollController.position.maxScrollExtent - scrollController.offset;
+    if ( threshold< 150 && currentChapterId < titles.length) {
+      Chapter ch = Chapter( currentChapterId + 1, novelId, titles[currentChapterId].title);
+      readChapters.addLast(ch);
+      getNovelDetail(ch);
+      currentChapterId++;
+    } else if (currentChapterId == titles.length && threshold < 50) {
+      Fluttertoast.showToast(
+          msg: "已经最后一章了",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIos: 2,
+          bgcolor: "#777777",
+          textcolor: '#ffffff');
+    }
   }
 
   /// 网络获取章节内容
@@ -190,7 +190,6 @@ class _ChapterDetailState extends State<ChapterDetail> {
         currentChapterId = index + 1;
         readChapters.clear();
         readChapters.addFirst(chapter);
-        print(readChapters.getLength());
         scrollController.jumpTo(0.0);
         getNovelDetail(chapter);
         Navigator.of(context).pop();
@@ -198,14 +197,21 @@ class _ChapterDetailState extends State<ChapterDetail> {
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
-            decoration: BoxDecoration(
-                border: Border(bottom: BorderSide(color: Colors.blueGrey))),
+            decoration: BoxDecoration( border: Border(bottom: BorderSide(color: Colors.blueGrey))),
             child: Text(chapter.title,
                 style: TextStyle(letterSpacing: 1.0, height: 1.2))),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
 
 /// 章节内容
 class Chapter {
