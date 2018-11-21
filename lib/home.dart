82c18ab7b7fee1ef71de8dc920f025616681c5c3
zdd <583627140@qq.com>
2018-11-21 10:58:06 +0800

    先删除讨论专区

--- lib/home.dart
+++ lib/home.dart
@@ -49,11 +49,11 @@ class HomeState extends State<Home> {
 
         /// 书库
         return BookLibrary();
+//      case 2:
+//
+//        /// 动弹
+//        return Talk();
       case 2:
-
-        /// 动弹
-        return Talk();
-      case 3:
 
         /// 我的
         return MySelf();
@@ -83,11 +83,11 @@ class HomeState extends State<Home> {
                   title: Text("书库"),
                   activeIcon: Icon(Icons.account_balance_wallet),
                   backgroundColor: Colors.blue),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.find_in_page),
-                  title: Text("发现"),
-                  activeIcon: Icon(Icons.find_in_page),
-                  backgroundColor: Colors.blue),
+//              BottomNavigationBarItem(
+//                  icon: Icon(Icons.find_in_page),
+//                  title: Text("发现"),
+//                  activeIcon: Icon(Icons.find_in_page),
+//                  backgroundColor: Colors.blue),
               BottomNavigationBarItem(
                   icon: Icon(Icons.person),
                   title: Text("我的"),
