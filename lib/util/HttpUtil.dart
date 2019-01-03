e845134bdfc2fb5e52ebf766fcd35b2224eec516
zdd <583627140@qq.com>
2019-01-03 17:47:40 +0800

    http请求重试间隔1秒，知道返回为止，

--- lib/util/HttpUtil.dart
+++ lib/util/HttpUtil.dart
@@ -4,29 +4,41 @@ import 'package:dio/dio.dart';
 
 class HttpUtil {
   static final Dio dio = new Dio();
+  static final Options options = new Options(connectTimeout: 3000,receiveTimeout: 3000);
 
   static Future<String> get(String url,{Map data}) async {
     print("get请求："+url);
     String result;
-    Response response=await dio.get(url,data: data);
-    if(response.statusCode == 200) {
+    try{
+      Response response=await dio.get(url,data: data,options: options);
+      if(response.statusCode == 200) {
         result = json.encode(response.data);
-    } else {
-      result = "Network Error\nHttp status ${response.statusCode}";
-      //result = null;
+      }else{
+        await Future.delayed(Duration(seconds: 1));
+        result = await get(url,data: data);
+      }
+    }catch(e){
+      await Future.delayed(Duration(seconds: 1));
+      result = await get(url,data: data);
     }
     return result;
   }
   static Future<String> post(String url,{Map data}) async {
     print("post请求："+url);
     String result;
-    Response response=await dio.post(url,data: data);
-    if(response.statusCode == 200) {
-        result = json.encode(response.data);
-    } else {
-      result = "Network Error\nHttp status ${response.statusCode}";
+    try{
+      Response response=await dio.post(url,data: data,options: options);
+      if(response.statusCode == 200) {
+          result = json.encode(response.data);
+      }else{
+        await Future.delayed(Duration(seconds: 1));
+        result = await post(url,data: data);
+      }
+    }catch(e){
+      await Future.delayed(Duration(seconds: 1));
+      result = await post(url,data: data);
     }
-    return result;
+     return result;
   }
   static download(String url,String savePath) async{
     print("download请求：$url");
